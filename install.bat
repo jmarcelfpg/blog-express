912778d93ac80704ca37d7cad73cc427bc541b22
jmarcelfpg <jmarcel_fpg@hotmail.com>
2018-02-28 14:29:19 -0800

    Converting from js to ts

---
+++ install.bat
@@ -0,0 +1,15 @@
+call npm install body-parser@latest
+call npm install cookie-parser@latest
+call npm install errorhandler@latest
+call npm install express@latest
+call npm install express-session@latest
+call npm install method-override@latest
+call npm install mongoose@latest
+call npm install morgan@latest
+call npm install pug@latest
+call npm install serve-favicon@latest
+call npm install stylus@latest
+call npm install expect.js@latest -D
+call npm install mocha@latest -D
+call npm install standard@latest -D
+call npm install superagent@latest -D
